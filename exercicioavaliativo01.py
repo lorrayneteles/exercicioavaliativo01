73b523d33b4cb400f9f1abe58fd2d085cbd3e691
lorrayneteles <lorrayneteles1305@gmail.com>
2024-10-04 20:45:22 -0300

    Add files via upload

---
+++ exercicioavaliativo01.py
@@ -0,0 +1,7 @@
+# Peça ao usuario dois numeros e mostre a soma deles
+n1 = int(input("Digite o primeiro número:"))
+n2 = int(input("Digite o segundo número:"))
+
+soma = ( n1 + n2)
+
+print(" A soma de dois números é:\n", soma)
